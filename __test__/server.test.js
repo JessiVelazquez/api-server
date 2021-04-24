ba39728c5dd2e865ecd2a32d5928407881e3ef80
Jessi <jessivelazq1@gmail.com>
2021-04-24 16:44:10 -0700

    got all server tests to pass

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -12,7 +12,7 @@ const mockRequest = supertest(server); // mock the server for us
 
 describe('------SERVER TESTS---------', () => {
 
-  // var foodTest = { name: 'burger', calories: '500', type: 'MEAT' };
+  //-----------------404 TESTS------------------\\
 
   it('should respond with a 404 on a bad route', async () => {
     return mockRequest.get('/where').then((data) => {
@@ -26,13 +26,41 @@ describe('------SERVER TESTS---------', () => {
     });
   });
 
+  //------------------CREATE TESTS--------------------\\
+
   it('should create a new item in the db', async () => {
     const response = await mockRequest.post('/food').send({ name: 'burger', calories: '500', type: 'MEAT' });
     expect(response.status).toBe(201);
     expect(response.body.type).toEqual('MEAT');
-    console.log('------POST TEST---------', response.body);
   });
 
+  it('should create a new item in the db', async () => {
+    const response = await mockRequest.post('/shoe').send({ name: 'shoetest', size: '9', type: 'W' });
+    expect(response.status).toBe(201);
+    expect(response.body.size).toEqual(9);
+  });
+
+  //-----------------READ ALL TESTS----------------------\\
+
+  it('should retrieve all items from the db', async () => {
+    const response = await mockRequest.get('/food');
+    expect(response.status).toBe(200);
+  });
+
+  it('should retrieve all items from the db', async () => {
+    const response = await mockRequest.get('/shoe');
+    expect(response.status).toBe(200);
+  });  
+
+  //-------------------READ ONE TESTS----------------------\\
+
+  it('should retrieve an item from the db', async () => {
+    let newShoe = await mockRequest.post('/shoe').send({ name: 'nike', size: '10', type: 'W' });
+    let id = newShoe.body._id;
+    const response = await mockRequest.get(`/shoe/${id}`);
+    expect(response.status).toBe(200);
+    expect(response.body.name).toEqual(newShoe.body.name);
+  });
 
   it('should retrieve an item from the db', async () => {
     let newFood = await mockRequest.post('/food').send({ name: 'burger2', calories: '500', type: 'MEAT' });
@@ -40,58 +68,48 @@ describe('------SERVER TESTS---------', () => {
     const response = await mockRequest.get(`/food/${id}`);
     expect(response.status).toBe(200);
     expect(response.body.name).toEqual(newFood.body.name);
-    console.log("------NEWFOOD---------", response.body.name, newFood.body.name);
   });
 
 
-  // it('should retrieve all items from the db', async () => {
-  //   const response = await mockRequest.get('/food');
-  //   expect(response.status).toBe(200);
-  // });
-
-  // it('should create a new item in the db', async () => {
-  //   const response = await mockRequest.post('/shoe').send({ name: 'brian' })
-  //   expect(response.status).toBe(201);
-  //   expect(response.body.record.name).toEqual('brian');
-  // });
-
-  // it('should retrieve an item from the db', async () => {
-  //   const response = await mockRequest.get('/shoe/1');
-  //   expect(response.status).toBe(200);
-  //   // expect(response.body).toBe(true);
-  //   console.log(response.body);
-  // });
-
-  // it('should retrieve all items from the db', async () => {
-  //   const response = await mockRequest.get('/shoe');
-  //   expect(response.status).toBe(200);
-  // });  
+  //----------------stretch - update tests------------------\\
 
 
-  // //stretch - update tests
+  it('should show that a shoe item was updated', async () => {
+    let newShoeItem = { name: 'reebok', size: '10', type: 'W' };
+    const origShoeItem = await mockRequest.post('/shoe').send({ name: 'merrell', size: '10', type: 'W' });
+    let id = origShoeItem.body._id;
+    let response = await mockRequest.put(`/shoe/${id}`).send(newShoeItem);
+    expect(response.status).toBe(200);
+    expect(response.body.name).toEqual(newShoeItem.name);
+  });
 
-  // it('should show that an item was updated', async () => {
-  //   const response = await mockRequest.put('/shoe/1').send({ name: 'jessi' });
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.record.name).toEqual('jessi');
-  // });
+  it('should show that a food item was updated', async () => {
+    let newFoodItem = { name: 'pizza', calories: '1000', type: 'MEAT' };
+    const origFoodItem = await mockRequest.post('/food').send({ name: 'salad', calories: '200', type: 'VEG' });
+    let id = origFoodItem.body._id;
+    let response = await mockRequest.put(`/food/${id}`).send(newFoodItem);
+    expect(response.status).toBe(200);
+    expect(response.body.name).toEqual(newFoodItem.name);
+  });
 
-  // it('should show that an item was updated', async () => {
-  //   const response = await mockRequest.put('/food/1').send({ name: 'jessi' });
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.record.name).toEqual('jessi');
-  // });
+  //----------------stretch - delete tests----------------------\\
 
-  // // stretch - delete tests
+  it('should show that a shoe was deleted', async () => {
+    let createShoe = await mockRequest.post('/shoe').send({ name: 'reebok', size: '10', type: 'W' });
+    let id = createShoe.body._id;
+    let response = await mockRequest.delete(`/shoe/${id}`);
+    expect(response.status).toBe(200);
+    let getResponse = await mockRequest.get(`/shoe/${id}`);
+    expect(getResponse.body).toBe(null);
+  });
 
-  // it('should show that an item was deleted', async () => {
-  //   const response = await mockRequest.delete('/shoe/1');
-  //   expect(response.status).toBe(200);
-  // });
-
-  // it('should show that an item was deleted', async () => {
-  //   const response = await mockRequest.delete('/food/1');
-  //   expect(response.status).toBe(200);
-  // });
+  it('should show that a food item was deleted', async () => {
+    let createFood = await mockRequest.post('/food').send({ name: 'sandwich', calories: '500', type: 'MEAT' });
+    let id = createFood.body._id;
+    let response = await mockRequest.delete(`/food/${id}`);
+    expect(response.status).toBe(200);
+    let getResponse = await mockRequest.get(`/food/${id}`);
+    expect(getResponse.body).toBe(null);
+  });
 
 });
